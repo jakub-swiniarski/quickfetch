e847924df8679b368bf5ce4a1a09f54efa8eefe0
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-11-25 15:16:50 +0100

    print labels

--- qf.c
+++ qf.c
@@ -2,10 +2,21 @@
 #include "config.h"
 
 int main(){
+    //get data
+    /*char data[HEIGHT][10];
+
+    FILE *fptr;
+    fptr=fopen(BATTERY,"r");
+    */
+
+    //print ascii logo and data
     for(int i=0; i<HEIGHT; i++){
-        for(int j=0; j<WIDTH; j++){
-            printf(ASCII_COLOR"%c", ASCII[i][j]);
-        }
+        //logo
+        printf(ASCII_COLOR"%s", ASCII[i]);
+        
+        //data
+        printf(" ");
+        printf("%s",LABELS[i]);
         printf(TEXT_COLOR);
         printf("\n");
     }
