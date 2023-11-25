c623ccfab1478025f0fea6e98b6198edc0a7c917
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-11-25 15:22:57 +0100

    print data

--- qf.c
+++ qf.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include "config.h"
 
+#define MAX 15
+
 int main(){
     //get data
-    /*char data[HEIGHT][10];
+    char data[HEIGHT][MAX];
+    for(unsigned short i=0; i<HEIGHT; i++)
+        sprintf(data[i], "ERROR");
 
     FILE *fptr;
     fptr=fopen(BATTERY,"r");
-    */
+    fgets(data[6],MAX,fptr); 
 
     //print ascii logo and data
-    for(int i=0; i<HEIGHT; i++){
+    for(unsigned short i=0; i<HEIGHT; i++){
         //logo
         printf(ASCII_COLOR"%s", ASCII[i]);
         
@@ -18,6 +22,7 @@ int main(){
         printf(" ");
         printf("%s",LABELS[i]);
         printf(TEXT_COLOR);
+        printf("%s",data[i]);
         printf("\n");
     }
 
