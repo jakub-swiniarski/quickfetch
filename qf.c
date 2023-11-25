7432b917a71427376e232704a93c4a3cbb2625af
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-11-25 14:43:47 +0100

    print ascii logo

--- qf.c
+++ qf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include "config.h"
 
 int main(){
-    printf("TEST");
+    for(int i=0; i<HEIGHT; i++){
+        for(int j=0; j<WIDTH; j++){
+            printf("%c",ASCII[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
