b3063a85afb7023283989aa87ea01c26d6d1c1ef
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2023-11-25 16:46:19 +0100

    read cpu temperature

--- qf.c
+++ qf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "config.h"
 
@@ -9,8 +10,18 @@ int main(){
         sprintf(data[i], "ERROR");
 
     FILE *fptr;
+
+    //cpu
+    fptr=fopen(CPU_TEMP,"r");
+    fgets(data[0], MAX, fptr);
+    int temp=atof(data[0]);
+    sprintf(data[0],"%d",temp/1000);
+    fclose(fptr);
+
+    //battery
     fptr=fopen(BATTERY,"r");
     fgets(data[6],MAX,fptr); 
+    fclose(fptr);
 
     //print ascii logo and data
     for(unsigned short i=0; i<HEIGHT; i++){
